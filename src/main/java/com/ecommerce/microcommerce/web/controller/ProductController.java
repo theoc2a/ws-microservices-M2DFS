f4033cbe52dc0739f12c7ff2533fd28748e7c2c2
theoc2a <theocussac2a@gmail.com>
2020-10-23 14:08:08 +0200

    Partie 1 fini et partie 0 manque l'update

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -11,40 +11,48 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Map;
+
+import java.util.HashMap;
 import java.util.List;
 
 
-
+@RestController
 public class ProductController {
 
     @Autowired
     private ProductDao productDao;
 
+    private static Map<Integer, Product> productList = new HashMap<Integer, Product>() {
+        {
+            put(1, new Product(1, "Ordinateur portable", 350, 230));
+            put(2, new Product(2, "Aspirateur Robot", 500, 300));
+            put(3, new Product(3, "Table de Ping Pong", 750, 350));
+        }
+    };
 
     //Récupérer la liste des produits
-    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
-    public MappingJacksonValue listeProduits() {
-        Iterable<Product> produits = productDao.findAll();
-        SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
-        FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
-        MappingJacksonValue produitsFiltres = new MappingJacksonValue(produits);
-        produitsFiltres.setFilters(listDeNosFiltres);
-        return produitsFiltres;
+    @GetMapping(value = "/Produits")
+    public Map<Integer, Product> listeProduits() {
+        return productList;
     }
 
 
     //Récupérer un produit par son Id
-    public Product afficherUnProduit() {
-        return null;
+    @GetMapping(value = "/afficherUnProduit/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        Product product = productList.get(id);
+        return product;
     }
-
-
-
 
     //ajouter un produit
     @PostMapping(value = "/Produits")
@@ -65,12 +73,30 @@ public class ProductController {
     }
 
     // supprimer un produit
-    public void supprimerProduit() {
+    @GetMapping(value = "/supprimerUnProduit/{id}")
+    public void supprimerProduit(@PathVariable int id) {
+        productList.remove(id);
     }
 
     // Mettre à jour un produit
-    public void updateProduit(@RequestBody Product product) {
-    }
+    @GetMapping(value = "/modifUnProduit/{id}/{type}/}{valeur}")
+    public void updateProduit(@PathVariable int id, @PathVariable int type, @PathVariable String valeur) {
+        Product product = productList.get(id);
+        Product productN = new Product();
+            if(type == 1) {
+                productN = product;
+                productN.setNom(valeur);
+                productList.entrySet();
+            }else if(type == 2) {
+                int i = Integer.parseInt(valeur);
+                product.setPrix(i);
+
+            }else if(type == 3) {
+                int i = Integer.parseInt(valeur);
+                product.setPrixAchat(i);
+
+            }
+        }
 
 
     //Pour les tests
@@ -79,6 +105,14 @@ public class ProductController {
         return productDao.chercherUnProduitCher(400);
     }
 
+        @GetMapping(value = "/margProduit/{id}")
+    public int calculerMargeProduit(@PathVariable int id){
+        Product product = productList.get(id);
+        int pProduct = product.getPrix();
+        int paProduct = product.getPrixAchat();
+        int marge = pProduct - paProduct;
 
+        return marge;
+    }
 
 }
