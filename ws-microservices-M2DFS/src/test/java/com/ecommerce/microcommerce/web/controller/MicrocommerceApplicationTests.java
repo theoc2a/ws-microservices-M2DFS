243eded3f75c799ae1826f1d708648f1c29ea255
theoc2a <theocussac2a@gmail.com>
2020-10-24 11:01:21 +0200

    Partie 5 partiellement fini

--- ws-microservices-M2DFS/src/test/java/com/ecommerce/microcommerce/web/controller/MicrocommerceApplicationTests.java
+++ ws-microservices-M2DFS/src/test/java/com/ecommerce/microcommerce/web/controller/MicrocommerceApplicationTests.java
@@ -39,19 +39,21 @@ public class MicrocommerceApplicationTests {
 	void testListeProduits() {
 		Map<Integer, Product> result = productController.listeProduits();
 		Assertions.assertEquals(new HashMap<Integer, Product>() {{
-			put(Integer.valueOf(0), new Product(0, "nom", 0, 0));
+			put(0, new Product(0, "Ordinateur portable", 350, 230));
+			put(1, new Product(1, "Aspirateur Robot", 500, 300));
+			put(2, new Product(2, "Table de Ping Pong", 750, 350));
 		}}, result);
 	}
 
 	@org.testng.annotations.Test
 	void testAfficherUnProduit() {
 		Product result = productController.afficherUnProduit(1);
-		Assertions.assertEquals(new Product(0, "nom", 0, 0), result);
+		Assertions.assertEquals(new Product(1, "Aspirateur Robot", 500, 300), result);
 	}
 
 	@org.testng.annotations.Test
 	void testAjouterProduit() {
-		productController.ajouterProduit("nom", 50, 1000);
+		productController.ajouterProduit("test", 50, 1000);
 	}
 
 	@org.testng.annotations.Test
@@ -67,7 +69,7 @@ public class MicrocommerceApplicationTests {
 	@org.testng.annotations.Test
 	void testCalculerMargeProduit() {
 		int result = productController.calculerMargeProduit(0);
-		Assertions.assertEquals(0, result);
+		Assertions.assertEquals(120, result);
 	}
 
 	@org.testng.annotations.Test
