d53c93c044fde40c5da45398b8190aea37e27d54
theoc2a <theocussac2a@gmail.com>
2020-10-24 11:35:37 +0200

    Partie 6 partiellement fini

--- circuitbreaker/src/main/java/com/ecommerce/circuitbreaker/circuitbreaker/CircuitbreakerApplication.java
+++ circuitbreaker/src/main/java/com/ecommerce/circuitbreaker/circuitbreaker/CircuitbreakerApplication.java
@@ -3,9 +3,12 @@ package com.ecommerce.circuitbreaker.circuitbreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class CircuitbreakerApplication {
 
 	public static void main(String[] args) {
--- ws-microservices-M2DFS/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ ws-microservices-M2DFS/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -3,9 +3,11 @@ package com.ecommerce.microcommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableSwagger2
+@Eureka
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
